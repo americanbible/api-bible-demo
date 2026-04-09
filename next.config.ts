6e9ee80d36c54e88382477b5517a017f5e590e97
Alex Burroughs <alex@deepspacerobots.com>
2026-04-09 18:12:46 -0500

    Setting up Amplify config

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
-  output: "export",
-  basePath: "/api-bible-demo",
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
