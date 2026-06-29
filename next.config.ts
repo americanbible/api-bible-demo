67493173d69237411d09582549a33ac124d29324
Alex Burroughs <alex@deepspacerobots.com>
2026-06-29 17:35:00 -0500

    Add all possible config options

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
+  cacheHandler: require.resolve("./utils/cacheHandler.cjs"),
   cacheHandlers: {
     default: require.resolve("./utils/cacheHandler.cjs"),
+  },
+  experimental: {
+    cacheHandlers: {
+      default: require.resolve("./utils/cacheHandler.cjs"),
+    },
   },
   cacheMaxMemorySize: 0,
 };
