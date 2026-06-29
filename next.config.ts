0d5d641aaf6ccc0213f5f567084726ccda536c30
Alex Burroughs <alex@deepspacerobots.com>
2026-06-29 17:20:57 -0500

    Sanity check

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
-  cacheHandler: require.resolve("./utils/cacheHandler.cjs"),
+  // cacheHandler: require.resolve("./utils/cacheHandler.cjs"),
   cacheMaxMemorySize: 0,
-  experimental: {
-    cacheHandlers: {
-      default: require.resolve("./utils/cacheHandler.cjs"),
-    },
-  },
+  // experimental: {
+  //   cacheHandlers: {
+  //     default: require.resolve("./utils/cacheHandler.cjs"),
+  //   },
+  // },
 };
 
 export default nextConfig;
